23bd53fddd0085de1baf9493a8d8bb8d512e84c7
Gerald <geraldzeng99@gmail.com>
2019-12-04 19:17:01 +0900

    Fixed to hide title bar and fixed status bar to transparency.

--- app/src/main/java/com/example/feedme/RegisterActivity.java
+++ app/src/main/java/com/example/feedme/RegisterActivity.java
@@ -1,22 +1,30 @@
 package com.example.feedme;
 
 import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
+
+import java.util.Objects;
 
 public class RegisterActivity extends AppCompatActivity {
     private Button mBtn_Register;
     private Button mBtn_Cancel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         mBtn_Register = findViewById(R.id.btn_register);
         mBtn_Cancel = findViewById(R.id.btn_cancel);
         mBtn_Register.setOnClickListener(new ButtonListen());
         mBtn_Cancel.setOnClickListener(new ButtonListen());
+        Objects.requireNonNull(getSupportActionBar()).hide();
     }
 
     private class ButtonListen implements View.OnClickListener {
