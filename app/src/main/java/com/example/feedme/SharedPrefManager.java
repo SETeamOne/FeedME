9c90151e2762f9105b1bf76e605cfe434471e50f
Gerald <geraldzeng99@gmail.com>
2019-12-12 23:06:40 +0900

    Add sonarqube config

--- app/src/main/java/com/example/feedme/SharedPrefManager.java
+++ app/src/main/java/com/example/feedme/SharedPrefManager.java
@@ -38,6 +38,16 @@ public class SharedPrefManager {
         return true;
     }
 
+    public boolean saveId(int id){
+        SharedPreferences sharedPreferences = ctx.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        editor.putInt(KEY_USER_ID, id);
+
+        editor.apply();
+        return true;
+    }
+
     public boolean isLoggedIn(){
         SharedPreferences sharedPreferences = ctx.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE);
         if(sharedPreferences.getString(KEY_USER_ID.toString(), null) != null){
